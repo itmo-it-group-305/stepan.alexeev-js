3b47b6bbab6adaeea3d96866069ad7727f219797
Garrus777rus <saren500@gmail.com>
2015-11-30 02:43:27 +0400

    Построение и вывод вариантов дерева... Постороить дерево заданной высоты пока не получается. Построить дерево из 2х "этажей" е получилось даже перебором.

--- lesson/js/functions.js
+++ lesson/js/functions.js
@@ -25,12 +25,60 @@ function returnObject(){
         document.write(i+' = '+ arguments[0][i]+'<br>');
     }
 }
-function createObject(){
+function createObject(val,rt){
     var obj={
-        value: null,
+        value: val,
         left: null,
         right: null,
-        root: null
+        root: rt
     };
     return obj;
+}
+function empty(objParam){
+    if(objParam==null)
+        return true
+     else
+        return false;
+}
+function chooseWhereToPlace(val,rt){
+   // console.log(rt);
+    if(val<rt.value && empty(rt.left)){
+        rt.left=createObject(val,rt);
+    }else if(val>rt.value && empty(rt.right)){
+        rt.right=createObject(val,rt);
+    }else if(!empty(rt.left)&& val<rt.value){
+        chooseWhereToPlace(val,rt.left);
+    }else if(!empty(rt.right) && val>rt.value){
+        chooseWhereToPlace(val,rt.right);
+    }
+  /*  if(val<rt.value){
+        if(notEmpty(rt.left)){
+            chooseWhereToPlace(val,rt.left);
+        }else{
+            rt.left=createObject(val,rt);
+        }
+    }else{
+        if(notEmpty(rt.left)){
+            chooseWhereToPlace(val,rt.right);
+        }else{
+            rt.right=createObject(val,rt);
+        }
+    }*/
+
+}
+function vivodDereva(obj1){
+    var obj1 = obj1;
+    var html='';
+        html+='<table width="100%" style="border: 1px solid #000; border-collapse: collapse; table-layout: auto" cellspacing="0" cellpadding="0">';
+        html+='<tr><td colspan="2" style="text-align:center; vertical-align: top;">'+ obj1.value +'</td></tr>';
+        if(obj1.left)
+            html+='<tr><td style="text-align:center; vertical-align: top; width:50%; border: 1px solid #000; border-collapse: collapse; box-sizing: border-box;">' + vivodDereva(obj1.left) + '</td>'
+        else
+            html+='<tr><td style="text-align:center; vertical-align: top; width:50%; border: 1px solid #000; border-collapse: collapse; box-sizing: border-box;">&nbsp;</td>';
+    if(obj1.right)
+            html+='<td style="text-align:center; vertical-align: top; width:50%; border: 1px solid #000; border-collapse: collapse; box-sizing: border-box;">' + vivodDereva(obj1.right) + '</td></tr>'
+        else
+            html+='<td style="text-align:center; vertical-align: top; width:50%; border: 1px solid #000; border-collapse: collapse; box-sizing: border-box;">&nbsp;</td></tr>';
+        html+='</table>';
+    return html;
 }
--- lesson/js/script.js
+++ lesson/js/script.js
@@ -17,8 +17,20 @@ document.write('<br><h1>Вывод объекта функцией</h1>');
 returnObject(myObj); //Вывод объекта функцией
 document.write('<br><h1>Вывод объекта своим же методом</h1>');
 myObj.returnThis(); //Вывод объекта методом обекта*/
+
 var a=[1,4,5,10,16,17,21];
-start = createObject();
-for(i=0; i< a.length; i++){
-    if()
+document.write('Дан ряд: [');
+var n;
+for(n=0; n< (a.length); n++){
+    document.write(a[n]+'  ');
+}
+n++;
+document.write(']');
+for(k=0; k< a.length; k++) {
+    document.write('<h1 style="text-align: center;"><br>Вершина - число '+a[k]+'</h1>');
+    rootObj = createObject(a[k], 'none');
+    for (i = 0; i < a.length; i++) {
+        chooseWhereToPlace(a[i], rootObj);
+    }
+    document.write(vivodDereva(rootObj));
 }
